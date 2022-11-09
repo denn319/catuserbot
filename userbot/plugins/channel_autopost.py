19c40594f7adb9748baaec8510d01c2c757522f7
DENN319 <davuth.den@gmail.com>
2022-11-09 10:59:37 +0700

    test events.album

--- userbot/plugins/channel_autopost.py
+++ userbot/plugins/channel_autopost.py
@@ -39,8 +39,8 @@ DST_CHANNEL_CAT = "all"  # can be any name. must exist using the .broadcast plug
 
 @catub.on(events.Album)
 async def auto_fwd(e):
+    LOGS.info(len(e.messages))
     LOGS.info(str(e))
-
 
 
 async def autopost(event):
