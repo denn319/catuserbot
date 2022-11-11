78cdf50f725b3fb4c39073e3917d3f9fa7cf2abb
denn319 <davuth.den@gmail.com>
2022-11-11 21:00:14 +0700

    new channel auto post plugin

--- userbot/plugins/channel_autopost.py
+++ userbot/plugins/channel_autopost.py
@@ -61,7 +61,7 @@ async def auto_albumfwd(e):
         # chats = sql.get_chat_broadcastlist(keyword)
 
         # LOGS.info(str(await e.get_input_chat()))
-        await e.send_message(int(TEST_CHANNEL_ID))
+        # await e.send_message(int(TEST_CHANNEL_ID))
         await e.forward_to(int(TEST_CHANNEL_ID))
         # catub.forward_messages(TEST_CHANNEL_ID, messages=events.Album)
         # LOGS.info(f"Message: {msg_id}")
