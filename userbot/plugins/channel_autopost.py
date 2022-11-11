4c85253121bbeaef015685edb8cb521a7cc68863
denn319 <davuth.den@gmail.com>
2022-11-11 20:56:18 +0700

    new channel auto post plugin

--- userbot/plugins/channel_autopost.py
+++ userbot/plugins/channel_autopost.py
@@ -26,10 +26,10 @@ TEST_CHANNEL_ID = "-1001890495163"
 
 
 class MyAlbum(events.Album):
-    def __int__(self, event):
-        super().__init__(event)
+    # def __int__(self, event):
+    #     super().__init__(event)
 
-    async def send_copy(self, *args, **kwargs):
+    async def send_message(self, *args, **kwargs):
         """
         Sends the entire album. Shorthand for
         `telethon.client.messages.MessageMethods.send_message`
@@ -61,7 +61,7 @@ async def auto_albumfwd(e):
         # chats = sql.get_chat_broadcastlist(keyword)
 
         # LOGS.info(str(await e.get_input_chat()))
-        await e.send_copy(int(TEST_CHANNEL_ID))
+        await e.send_message(int(TEST_CHANNEL_ID))
         await e.forward_to(int(TEST_CHANNEL_ID))
         # catub.forward_messages(TEST_CHANNEL_ID, messages=events.Album)
         # LOGS.info(f"Message: {msg_id}")
